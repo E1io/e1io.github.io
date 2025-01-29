ec7e7fa58bbff3cf86389ad5b862fdf9f175fe8b
e1io <ejorgensen.dearborn@gmail.com>
2025-01-28 21:36:56 -0500

    first commit

--- js/downloadPDF.js
+++
@@ -1,8 +0,0 @@
-function downloadPDF() {
-    const link = document.createElement('a');
-    link.href = 'Black Modern Professional Resume.pdf'; 
-    link.download = 'Elijah_Jorgensen.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-   }
