e5a79b186e879c98c3d7ada67f5ba31ddd4264cd
e1io <ejorgensen.dearborn@gmail.com>
2025-02-03 15:41:43 -0500

    fix main page images

--- js/quote.js
+++ js/quote.js
@@ -1,9 +1,9 @@
 const images = [
-    "assets/quote1.webp", "assets/quote2.webp", "assets/quote3.webp",
-    "assets/quote4.webp", "assets/quote5.webp", "assets/quote6.webp",
-    "assets/quote7.webp", "assets/quote8.webp", "assets/quote9.webp",
-    "assets/quote10.webp", "assets/quote11.webp", "assets/quote12.webp",
-    "assets/quote13.webp","assets/quote14.webp"
+    "assets/webp/quote1.webp", "assets/webp/quote2.webp", "assets/webp/quote3.webp",
+    "assets/webp/quote4.webp", "assets/webp/quote5.webp", "assets/webp/quote6.webp",
+    "assets/webp/quote7.webp", "assets/webp/quote8.webp", "assets/webp/quote9.webp",
+    "assets/webp/quote10.webp", "assets/webp/quote11.webp", "assets/webp/quote12.webp",
+    "assets/webp/quote13.webp","assets/webp/quote14.webp"
 ];
 //https://www.codecademy.com/resources/blog/inspirational-coding-quotes/
 
